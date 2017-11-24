0b693583282f60735f0b456b9b667563da7d564f
duynguyen212 <duynguyen212@gmail.com>
2017-11-24 15:24:59 +0100

    Add Code For Product

--- php/stock_system/php_action/createProduct.php
+++ php/stock_system/php_action/createProduct.php
@@ -5,11 +5,25 @@
 
 	if ($_POST) {
 		$productName = $_POST['productName'];
-		$productName = $_POST['productName'];
-		$productName = $_POST['productName'];
-		$productName = $_POST['productName'];
-		$productName = $_POST['productName'];
-		$productName = $_POST['productName'];
-		$productName = $_POST['productName'];
+		$productQuantity = $_POST['productQuantity'];
+		$productRate = $_POST['productRate'];
+		$productBrandName = $_POST['productBrandName'];
+		$productCategoryName = $_POST['productCategoryName'];
+		$productStatus = $_POST['productStatus'];
+		
+		//for product image
+		$type = explode('.', $_FILES['productImage']['name']);
+		$type = $type[count($type) - 1];
+		$url = "../assets/images/stock/" .uniqid(rand()) . '.' . $type;
+		if (in_array($type, array('gif', 'jpg', 'jpeg', 'png', 'PNG', 'GIF', 'JPG', 'jpeg'))){
+			if(is_uploaded_file($_FILES['productImage']['tmp_name'])) {
+				if(move_uploaded_file($_FILES['productImage']['tmp_name'], $url)){
+					$valid['success'] = true;
+					$valid['message'] = "Successfully Added";
+				} else {
+					return false;
+				}
+			}
+		}
 	}
 ?>
