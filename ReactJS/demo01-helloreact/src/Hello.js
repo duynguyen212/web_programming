57b16be71026e07315c1011d3298d9b49333151d
duynguyen212 <duynguyen212@gmail.com>
2018-03-18 20:35:41 +0100

    ReactJS Demo

---
+++ ReactJS/demo01-helloreact/src/Hello.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Hello extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Bonjour ReactJS</h1>
+        <p>Bienvenue au cour de ReactJS</p>
+      </div>
+    );
+  }
+}
+
+export default Hello;
