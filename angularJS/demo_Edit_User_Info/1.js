f5d1a3beaf44e8b2d879b24f81ad038c378a4908
Duy NGUYEN <duynguyen212@gmail.com>
2019-04-19 12:50:50 +0200

    Using ng-repeat, ng-show to Demo User's Info Edit

---
+++ angularJS/demo_Edit_User_Info/1.js
@@ -0,0 +1,31 @@
+﻿ var app = angular.module('myApp',['ngMaterial']);
+ app.controller('MyController',  function($scope){
+ 	$scope.users = [
+ 		{
+ 			name: "Lionel Messi", 
+ 			yearofbirth: "1988",
+ 			facebook: "fb.com/lionelmessi"
+ 		},
+ 		{
+ 			name: "Cristiano Ronaldo", 
+ 			yearofbirth: "1986",
+ 			facebook: "fb.com/cr7"
+ 		},
+ 		{
+ 			name: "Kylian Mbappe", 
+ 			yearofbirth: "2001",
+ 			facebook: "fb.com/k10"
+ 		},
+ 		{
+ 			name: "Noname", 
+ 			yearofbirth: "1988",
+ 			facebook: "fb.com/nameno"
+ 		}
+ 	];
+
+ 	//$scope.editUserDisplay = false;
+
+ 	$scope.onEditUser = function(userObj) {
+ 		userObj.editUser = !userObj.editUser;
+ 	}
+ })
